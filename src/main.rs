c675eb1cf116e3871961d911d57d23c945be2851
Jayslen <jayslen134@gmail.com>
2026-04-02 16:35:19 -0400

    read the width, height and bits per pixels of the image. Move bytes reading into method of struct and create funtion to read le bytes

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use std::{
 };
 
 struct BMP {
-    size: usize,
-    starting_adress: usize,
+    size: u32,
+    starting_adress: u32,
+    width: u32,
+    height: u32,
+    bits_per_pixel: u16,
 }
 
 impl BMP {
@@ -16,10 +19,20 @@ impl BMP {
             return format!("{:.1} KB", self.size as f64 / 1024_f64);
         }
     }
+
+    fn init_info(bmp_header: &[u8; 14], dib_header: &[u8; 40]) -> BMP {
+        BMP {
+            size: read_le_bytes_u32(&bmp_header[2..6]),
+            starting_adress: read_le_bytes_u32(&bmp_header[10..]),
+            width: read_le_bytes_u32(&dib_header[4..8]),
+            height: read_le_bytes_u32(&dib_header[8..12]),
+            bits_per_pixel: read_le_bytes_u16(&dib_header[14..16]),
+        }
+    }
 }
 
 fn main() {
-    let file = File::open("./test_images/sample_640×426.bmp").unwrap();
+    let file = File::open("./test_images/square.bmp").unwrap();
 
     // read the first two bytes of the file to verify if its a bmp image
     // The first 2 must be "BM" for a bmp image
@@ -27,25 +40,37 @@ fn main() {
     let mut bmp_header: [u8; 14] = [0; 14];
 
     buffer_reader
-        .read_exact(&mut bmp_header[0..2])
-        .expect("Error while reading the signature");
+        .read_exact(&mut bmp_header)
+        .expect("Error while reading the header");
 
-    let signature_chars = str::from_utf8(&bmp_header[0..2]).unwrap();
+    let signature_chars = str::from_utf8(&bmp_header[..2]).unwrap();
 
     if signature_chars != "BM" {
         panic!("This is not a bmp image")
     }
 
+    let mut bitmap_info_header_buffer: [u8; 40] = [0; 40];
     buffer_reader
-        .read_exact(&mut bmp_header[2..])
-        .expect("Error while reading the header");
+        .read_exact(&mut bitmap_info_header_buffer)
+        .expect("Errors while reading dib header");
 
-    let image: BMP = BMP {
-        size: u32::from_le_bytes(bmp_header[2..6].try_into().unwrap()) as usize,
-        starting_adress: u32::from_le_bytes(bmp_header[10..].try_into().unwrap()) as usize,
-    };
+    let image = BMP::init_info(&bmp_header, &bitmap_info_header_buffer);
+}
 
-    println!("Size of image: {}", image.get_size());
-    println!("Starting address of image data: {}", image.starting_adress);
-    // get size of image
+fn read_le_bytes_u32(buffer: &[u8]) -> u32 {
+    let bytes = buffer
+        .try_into()
+        .inspect_err(|e| eprintln!("Something went wrong {:?}", e))
+        .unwrap();
+
+    u32::from_le_bytes(bytes)
+}
+
+fn read_le_bytes_u16(buffer: &[u8]) -> u16 {
+    let bytes = buffer
+        .try_into()
+        .inspect_err(|e| eprintln!("Something went wrong {:?}", e))
+        .unwrap();
+
+    u16::from_le_bytes(bytes)
 }
